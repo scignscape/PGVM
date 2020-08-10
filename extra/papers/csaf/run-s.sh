d40a244b7f3e74cb3f7739623cf47c7613cfb592
scignscape-rz <osrworkshops@gmail.com>
2020-08-10 16:56:28 -0400

    update

--- extra/papers/csaf/run-s.sh
+++ extra/papers/csaf/run-s.sh
@@ -1,3 +1,3 @@
 ar=`git rev-parse --show-toplevel`
-`cat $ar/../preferred/preferred-pdflatex` -interaction=nonstopmode --shell-escape SDRM.tex
+`cat $ar/../preferred/preferred-pdflatex` -interaction=nonstopmode --shell-escape SDRF.tex
 
